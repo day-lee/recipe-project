99ba5da7732bb0628b0f1b070d2e305334b74c5b
Dayeon Lee <dayeonlee@mac.lan>
2025-08-15 10:56:39 +0100

    Add tag filter

--- app/(routes)/recipes/components/TagList.tsx
+++ app/(routes)/recipes/components/TagList.tsx
@@ -3,16 +3,23 @@ import { createClient } from '@/lib/supabase/server'
 export default async function TagList() {
     const supabase = await createClient()
     const { data: tags, error } = await supabase.from('tag').select('id, tag_name');
-    
-    if (error) {
+      if (error) {
       console.error('Error fetching tags:', error);
       return <div>Error loading tags</div>;
-    }   
-    return (
-      <div>
-        <h1 className="text-2xl font-bold mt-10">Tags</h1>
-        {tags?.map(tag => <div key={tag.id}>{tag.tag_name}</div>)}
-      </div>
-    );
+    }  
+    console.log(tags)
+    return tags 
+    // return (
+    //   <div className='flex'>
+    //      {tags?.map(tag => <button className='bg-blue-500 m-2 p-1' key={tag.id}> {tag.tag_name}</button>)}
+    //   </div>
+    // );
   }
   
+  /*
+  Change to client component
+  bc, interactivity for button clicks 
+  handleFilter and set tag as params - same logic as search.tsx
+  
+  issue: would it chain automatically with & 
+  */
