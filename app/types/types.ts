ddd38d28849ded67bd66eaf09649e9594ad927cd
Dayeon Lee <iamdayeonlee@gmail.com>
2025-08-22 10:48:17 +0100

    Add recipe detail page with css

--- app/types/types.ts
+++ app/types/types.ts
@@ -4,9 +4,32 @@ export interface Tag {
     recipe_count: number;
   }
 
+  export interface Step {
+    step: number;
+    photo_id: number;
+    desc: string;
+  }
+
 export interface Recipe {
     id: number;
     recipe_name: string;
     duration: number;
     img_link: string;
   }  
+
+  export interface Ingredient {
+    id: number;
+    recipe_id: number;
+    recipe_name: string;
+    ingredient_name: string;
+    quantity: number;
+    unit: string;
+    is_main: boolean;
+    is_optional: boolean;
+    is_sauce: boolean;
+  }
+
+  export interface Note {
+    id: number;
+    desc: string;
+  }
