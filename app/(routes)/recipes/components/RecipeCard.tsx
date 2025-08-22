f5d44c0abddfb87879adad85886fcaed088f9102
Dayeon Lee <iamdayeonlee@gmail.com>
2025-08-22 21:28:26 +0100

    Add Link and hover effect to recipe cards

--- app/(routes)/recipes/components/RecipeCard.tsx
+++ app/(routes)/recipes/components/RecipeCard.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link'
+
 import { Recipe } from '../../../types/types'
 import fallbackImg from '../../../assets/unavailable.png'
 
 export default function RecipeCard({recipe}: {recipe:Recipe}) {
 return(
     <>
-        <div className='border-slate-600 shadow-md bg-white w-[230px]'>
+    <Link href={`/recipes/${recipe.id}`}>
+        <div className='border-slate-600 shadow-md bg-white w-[230px] transition-all duration-300 hover:shadow-lg hover:-translate-y-1'>
             {recipe.img_link ? 
             <Image className='w-full' priority={true} src={recipe.img_link} alt={recipe.recipe_name} width={200} height={150}/> :
             <Image className="w-full" src={fallbackImg} alt='fallbackImg' width={200} height={150}/>
@@ -15,6 +18,7 @@ return(
                 <div className='text-gray-600 text-sm'> {recipe.duration} mins </div>
             </div>
         </div>
+    </Link>
     </>
 )
 }
