f8f6a48d7122ff20048e5265a44922821f62c463
Dayeon Lee <iamdayeonlee@gmail.com>
2025-08-22 23:39:25 +0100

    Add fallback message for empty tag click

--- app/(routes)/recipes/components/RecipeList.tsx
+++ app/(routes)/recipes/components/RecipeList.tsx
@@ -3,12 +3,19 @@ import RecipeCard from './RecipeCard';
 
 export default async function RecipeList({ recipes}: {recipes: Recipe[]}) {
     return (
-      <div className='flex-grow'>
-        <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
-          {recipes?.map(recipe => (
-            <li key={recipe.id}> <RecipeCard recipe={recipe}/> </li>
-          ))}
-        </ul>
-      </div>
+      <>
+      { recipes.length > 0 ?
+        (<div className='flex-grow'>
+          <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+            {recipes?.map(recipe => (
+              <li key={recipe.id}> <RecipeCard recipe={recipe}/> </li>
+            ))}
+          </ul>
+        </div>) 
+        : 
+        (<div> 
+            <p className='flex justify-center items-center font-bold'> There is nothing in this tag yet—start adding your recipes! </p>
+        </div>)}
+    </>
     );
   }
