ba754793f8f648c91f273c84e9a399d60621e2e8
Dayeon Lee <iamdayeonlee@gmail.com>
2025-09-27 23:09:10 +0100

    Refactor the test

--- __tests__/app/(routes)/new-recipe/components/NewRecipeForm.test.tsx
+++ __tests__/app/(routes)/new-recipe/components/NewRecipeForm.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { NewRecipeForm } from '@/app/(routes)/new-recipe/components/NewRecipeForm'
 import  { createRecipeAction } from '@/app/actions'
@@ -18,20 +19,22 @@ describe('RecipeForm', () => {
     it('should display a validation error if name is empty', async () => {
         render(<NewRecipeForm tags={mockTags} />);
         const createBtn = screen.getByRole('button', {name: /Create/i });
-        fireEvent.click(createBtn);
-        const errorMsg = await screen.findByText('Please add a name for your recipe.')
-        expect(errorMsg.toBeInTheDocument());
+        await userEvent.click(createBtn);
+        const errorMsg = screen.findByText('Recipe name must be at least 3 characters long')
+        expect(errorMsg).toBeInTheDocument()
+        
     })
 
-    it('should display tags', )
+
+    // it('should display tags', )
     
-    it('should add more ingredients when "add more" button is clicked', )
-    it('should clear the input when "remove" button is clicked', )
-    // same for steps and notes
-    it('should add youtube thumbnail when a valid youtube link is added',)
-    it('should remove the youtube thumbnail when "remove" button is clicked',)
-    it('should submit the form when all required fields are filled', )
-    it('should display an error message when the server action fails',)
+    // it('should add more ingredients when "add more" button is clicked', )
+    // it('should clear the input when "remove" button is clicked', )
+    // // same for steps and notes
+    // it('should add youtube thumbnail when a valid youtube link is added',)
+    // it('should remove the youtube thumbnail when "remove" button is clicked',)
+    // it('should submit the form when all required fields are filled', )
+    // it('should display an error message when the server action fails',)
     
 
 
