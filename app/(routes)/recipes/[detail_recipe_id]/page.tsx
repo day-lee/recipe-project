608cae8789d6609854a1b255259418d51fe52a1c
Dayeon Lee <iamdayeonlee@gmail.com>
2025-10-19 15:53:54 +0100

    Refactor to move page rpc into action

--- app/(routes)/recipes/[detail_recipe_id]/page.tsx
+++ app/(routes)/recipes/[detail_recipe_id]/page.tsx
@@ -1,26 +1,28 @@
-import { createClient } from '@/lib/supabase/server'
+import Link from 'next/link';
 
 import DetailCard from '@/app/(routes)/recipes/[detail_recipe_id]/components/DetailCard'
+import { getDetailRecipeWithTag, getIngredients } from '@/app/(routes)/recipes/actions';
 
 export default async function Page({ params }: {
     params: Promise<{ detail_recipe_id: string }>
 }) {
-const { detail_recipe_id } = await params     
-const supabase = await createClient()
-const { data: recipeDetail, error:recipeDetailError } = await supabase
-        .rpc('get_detail_recipe_with_tag', { detail_recipe_id });
-if (recipeDetailError) {
-    console.error('Error fetching recipes:', recipeDetailError);
-    return <div>Error loading recipes</div>;
-  }   
-const { id } = recipeDetail[0];
-const { data: ingredients, error:recipeIngredientError } = await supabase
-        .rpc('get_ingredients', { ingredient_recipe_id: id});
-if (recipeIngredientError) {
-    console.error('Error fetching recipe ingredients:', recipeIngredientError);
-    return <div>Error loading ingredients</div>;
-  }    
-    return (
-        <DetailCard recipeDetail={recipeDetail[0]} ingredients={ingredients} />
-    )
+const { detail_recipe_id: recipe_public_id } = await params     
+const { data: recipeDetail, error: recipeError } = await getDetailRecipeWithTag(recipe_public_id)
+const { id } = recipeDetail!;
+const { data: ingredients, error: ingredientError } = await getIngredients(id)
+
+if (recipeError || ingredientError || !recipeDetail || !ingredients) {
+  return (
+    <div className='flex flex-col items-center mt-20'> Cannot find the recipe. 
+      <Link 
+        href="/recipes" 
+        className="mt-4 text-blue-500 hover:underline">
+        Back to recipe list
+      </Link>
+    </div>
+  )
+} 
+  return (
+    <DetailCard recipeDetail={recipeDetail} ingredients={ingredients} />
+  )
 }
--- app/(routes)/recipes/actions.ts
+++ app/(routes)/recipes/actions.ts
@@ -5,7 +5,7 @@ import { PostgrestError } from '@supabase/supabase-js';
 
 import { recipeSchema } from "@/app/utils/validation/recipe";
 import { createClient } from '@/lib/supabase/server'
-import { FormSubmitData, MainListIngredientTag } from '@/app/types/types'
+import { FormSubmitData, MainListIngredientTag, RecipeDetail } from '@/app/types/types'
 import { mergeIngredients } from "@/app/utils/utils";
 
 export async function getMainIngredientTags(): Promise<{ data: MainListIngredientTag[] | null; error: PostgrestError | null}> {
@@ -58,3 +58,26 @@ export async function createRecipe(payload: FormSubmitData) {
     }}
     return {success: true, data };
   }
+
+  export async function getDetailRecipeWithTag(detail_recipe_id: string) {
+    const supabase = await createClient()
+    const { data: recipeDetail, error:recipeDetailError } = await supabase
+            .rpc('get_detail_recipe_with_tag', { detail_recipe_id });
+    if (recipeDetailError) {
+        console.error('Error fetching recipe detail with tag:', recipeDetailError);
+        return {data: null, error: recipeDetailError};
+      }   
+    const recipeDetailResult = Array.isArray(recipeDetail) ? recipeDetail[0] : recipeDetail  
+      return {data: recipeDetailResult as RecipeDetail, error: null};
+  }
+
+export async function getIngredients(ingredient_recipe_id: number) {
+    const supabase = await createClient();
+    const { data: ingredients, error:recipeIngredientError } = await supabase
+            .rpc('get_ingredients', { ingredient_recipe_id: ingredient_recipe_id});
+    if (recipeIngredientError) {
+        console.error('Error fetching recipe ingredients:', recipeIngredientError);
+        return {data: null, error: recipeIngredientError};
+      }    
+    return {data: ingredients, error: null};  
+}
