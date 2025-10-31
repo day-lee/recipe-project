4f0c7e25e12d0aa3ce12bc1b2e10d5f38f6041ab
Dayeon Lee <iamdayeonlee@gmail.com>
2025-10-31 20:33:39 +0000

    Design front page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { AuthButton } from "@/components/auth-button";
 // import { LoginForm } from "@/components/login-form";
 import Link from 'next/link'
 import { catchPhrase } from "./(routes)/recipes/components/mainPage/MainSearchFilter";
+import icon from '@/app/icon.svg'
+import Image from "next/image";
 
 const ctaButton = 'Discover All Amazing Recipes'
 export default async function Home() {
@@ -20,7 +22,11 @@ export default async function Home() {
         <div className="flex-1 flex flex-col gap-10 max-w-5xl p-5 items-center">
           <div className="flex flex-col items-center">
             <p className="text-5xl sm:text-8xl font-semibold text-red-800 mb-4 sm:mb-8 font-serif ">Bon Appétit,</p>
-            <p className="text-lg sm:text-2xl text-gray-600 text-center">{catchPhrase}</p>
+            <p className="text-lg sm:text-2xl text-gray-600 text-center font-normal">{catchPhrase}</p>
+            <div className="flex items-center mt-1 sm:mt-4">
+              <Image src={icon} className="w-8 h-8" alt="logo"/>
+              <p className="text-lg sm:text-2xl text-red-700 font-medium p-1">Recipe Project</p>
+            </div>
           </div>
           <div className="flex flex-col px-4 py-8">
               <Link href={`/recipes`}>
