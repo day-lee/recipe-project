160b7d351a7378d0be2c4b2cfcaa2d5c175340ac
Dayeon Lee <iamdayeonlee@gmail.com>
2025-11-06 14:13:01 +0000

    Refactor button hover colour to match mobile

--- app/(routes)/recipes/components/mainPage/MainIngTagButton.tsx
+++ app/(routes)/recipes/components/mainPage/MainIngTagButton.tsx
@@ -11,7 +11,7 @@ export default function MainIngTagButton({ mainIngredientTags, selectedMainIngTa
             <div key={tag.id} className="mx-1 ">
                 <button
                     className={`flex items-center m-1 px-2 h-8 text-sm text-black font-medium rounded-full active:scale-95 
-                                hover:bg-red-100 hover:text-red-700 ${
+                                hover:bg-red-700 hover:text-white ${
                         selectedMainIngTagId === tag.id ? 'text-white bg-red-700' : 'bg-stone-300'}`}
                     onClick={() => onClick?.(tag.id, tag.tag_name)}>
                     {tag.tag_name} {tag.recipe_count > 0 ? `(${tag.recipe_count})` : '(0)'}
