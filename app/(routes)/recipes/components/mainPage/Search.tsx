01348581ca2e4394618f7f8357de262e49d49353
Dayeon Lee <iamdayeonlee@gmail.com>
2025-11-07 18:43:15 +0000

    Add typing animation for search placeholder

--- app/(routes)/recipes/components/mainPage/Search.tsx
+++ app/(routes)/recipes/components/mainPage/Search.tsx
@@ -1,8 +1,70 @@
 'use client';
-
+import { useState, useEffect } from 'react';
 import { SearchProps } from '@/app/types/types'
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
+function useLoopingTyping(
+    placeholders: string[] = [],   
+    typeSpeed = 150,     
+    deleteSpeed = 50,    
+    pauseTime = 1000     
+):string {
+  const [placeholder, setPlaceholder] = useState('');
+  const [textIdx, setTextIdx] = useState(0);
+  const [charIdx, setCharIdx] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  useEffect(() => {
+    if (!placeholders.length) return;
+
+    const currentText = placeholders[textIdx];
+    let timeoutId: ReturnType<typeof setTimeout>;
+
+    if (!isDeleting) {
+      // typing mode
+      if (charIdx <= currentText.length) {
+        timeoutId = setTimeout(() => {
+          setPlaceholder(currentText.slice(0, charIdx));
+          setCharIdx(charIdx + 1);
+        }, typeSpeed);
+      }
+      // finishing typing, pause before deleting
+      if (charIdx === currentText.length + 1) {
+        timeoutId = setTimeout(() => setIsDeleting(true), pauseTime);
+      }
+    } else {
+      // deleting mode
+      if (charIdx >= 0) {
+        timeoutId = setTimeout(() => {
+          setPlaceholder(currentText.slice(0, charIdx));
+          setCharIdx(charIdx - 1);
+        }, deleteSpeed);
+      }
+      // all deleted, move to next string
+      if (charIdx === -1) {
+        setIsDeleting(false);
+        setTextIdx((textIdx + 1) % placeholders.length);
+        setCharIdx(0);
+      }
+    }
+    return () => clearTimeout(timeoutId);
+  }, [
+    placeholders,
+    textIdx,
+    charIdx,
+    isDeleting,
+    typeSpeed,
+    deleteSpeed,
+    pauseTime,
+  ]);
+  return placeholder;
+}
+
 export default function Search({onChange, searchInput}:SearchProps ) {
+      const placeholder = useLoopingTyping(
+    ['kimchi', 'english breakfast', 'green curry', 'hainanese chicken rice', 'beef stew'],
+    100, 50, 800
+  );
     return (
         <div className='flex'>
             <div className="relative w-5/6">
@@ -12,7 +74,7 @@ export default function Search({onChange, searchInput}:SearchProps ) {
                 type="text"
                 onChange={(e) => onChange?.(e.target.value)}
                 value={searchInput}
-                placeholder="Search recipes"
+                placeholder={placeholder}
             />
             <MagnifyingGlassIcon className="absolute left-4 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500" />
             </div>
