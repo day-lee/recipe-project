0ce91f156666058fd0df5e87b8389be6525fe97e
Dayeon Lee <iamdayeonlee@gmail.com>
2026-04-03 23:00:56 +0100

    Refactor to remove excessive margin bottom of page title

--- app/(routes)/recipes/new/page.tsx
+++ app/(routes)/recipes/new/page.tsx
@@ -16,7 +16,7 @@ export default async function NewRecipeFormPage() {
       <h1 className="text-2xl font-bold text-center">
         Create a new recipe
       </h1>
-      <p className='pb-4'>Share your yummy ideas!</p>
+      <p>Share your yummy ideas!</p>
       <RecipeForm mainIngredientTag={mainIngredientTag || []} mode="create" userId={user} />
     </main>
   );
